f2956a3ade4191566c5256c011b13fa7317a77db
Re1o <reio.reimers@hotmail.com>
2020-02-16 15:14:47 +0200

    2.16.20

---
+++ MatrjoshkaProject/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatrjoshkaProject
+{
+    class Program
+    {
+        class Matrjoshka
+        {
+            public static int Count = 0;
+            string name;
+            string color;
+            int size;
+            string image;
+
+            public Matrjoshka(string _name, string _color, int _size, string _image)
+            {
+                name = _name;
+                color = _color;
+                Size = _size;
+                image = _image;
+                Count++;
+            }
+
+            public string Color
+            {
+                get { return color; }
+            }
+
+            public string Image
+            {
+                get { return image; }
+            }
+
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public int Size
+            {
+                get { return size; }
+                set
+                {
+                    if (value > 0 && value <= 10)
+                    {
+                        size = value;
+                    }
+                    else
+                    {
+                        size = 1;
+                    }
+                }
+            }
+
+            public Matrjoshka OpenMatrjoshka(string name, string color, string image)
+            {
+                Matrjoshka newMatrjoshka = new Matrjoshka(name, color, size - 2, image);
+                return newMatrjoshka;
+            }
+
+
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            List<Matrjoshka> bogOfMatrioskas = new List<Matrjoshka>();
+            Matrjoshka matrioska1 = new Matrjoshka("Matrioska1", "green", 10, "image1");
+            bogOfMatrioskas.Add(matrioska1);
+            //open matrioska
+            Matrjoshka matrioska2 = matrioska1.OpenMatrjoshka("Matrioska2", "orange", "image2");
+            bogOfMatrioskas.Add(matrioska2);
+
+            Matrjoshka matrioska3 = matrioska2.OpenMatrjoshka("Matrioska3", "pink", "image3");
+            bogOfMatrioskas.Add(matrioska3);
+
+            Matrjoshka matrioska4 = matrioska3.OpenMatrjoshka("Matrioska4", "yellow", "image4");
+            bogOfMatrioskas.Add(matrioska4);
+
+            Matrjoshka matrioska5 = matrioska4.OpenMatrjoshka("Matrioska5", "blue", "image5");
+            bogOfMatrioskas.Add(matrioska5);
+
+            foreach (Matrjoshka matrioska in bogOfMatrioskas)
+            {
+                Console.WriteLine($"A {matrioska.Color} {matrioska.Name} is in the box ");
+            }
+
+            Console.WriteLine($"there are {Matrjoshka.Count} matrioskas in the box");
+
+            //take a matrioska from the box
+            Console.WriteLine("What color matrioska would you take from the box?");
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i < bogOfMatrioskas.Count; i++)
+            {
+                
+
+                if (bogOfMatrioskas[i].Color == userInput)
+                {
+                    Console.WriteLine($"You have taken {bogOfMatrioskas[i].Name} from the box.");
+                    bogOfMatrioskas.Remove(bogOfMatrioskas[i]);
+                    break;
+                }
+                
+            }
+            foreach(Matrjoshka matrjoshka in bogOfMatrioskas)
+            {
+                
+                Console.WriteLine($"Name: {matrjoshka.Name}, color: {matrjoshka.Color} is in the box");
+            }
+
+
+        }
+    }
+}
---
+++ Movie/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Movie
+{
+    class Program
+    {
+        class MovieClass
+        {
+            string title;
+            string director;
+            string ratings;
+            int userRating;
+
+            public MovieClass(string _title, string _director, string _ratings, int _userRating)
+            {
+                title = _title;
+                director = _director;
+                Ratings = _ratings;
+                UserRating = _userRating;
+            }
+
+            public string Ratings
+            {
+                get { return ratings; }
+                set
+                {
+                    if (value == "G" || value == "PG" || value == "PG-13" || value == "R")
+                    {
+                        ratings = value;
+                    }
+                    else
+                    {
+                        ratings = "undefined";
+                    }
+                }
+            }
+
+            public int UserRating
+            {
+                get { return userRating; }
+                set
+                {
+                    if (value >= 0 && value <= 10)
+                    {
+                        userRating = value;
+                    }
+                    else
+                    {
+                        userRating = 0;
+                    }
+                }
+            }
+
+            public string Title
+            {
+                get { return title; }
+            }
+
+            public string Director
+            {
+                get { return director; }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\demo\Movies.txt";
+            List<string> listFromFile = File.ReadAllLines(filePath).ToList();
+            List<MovieClass> listOfMovies = new List<MovieClass>();
+
+            foreach (string line in listFromFile)
+            {
+
+
+                string[] tempArray = line.Split('/');
+                string tempTitle = tempArray[0];
+                string tempDir = tempArray[1];
+                string tempRatting = tempArray[2];
+                int tempUserRating = int.Parse(tempArray[3]);
+
+                MovieClass tempMovieObject = new MovieClass(tempTitle, tempDir, tempRatting, tempUserRating);
+                listOfMovies.Add(tempMovieObject);
+
+            }
+
+            int i = 1;
+
+            foreach (MovieClass movieObject in listOfMovies)
+            {
+                Console.WriteLine($"item {i}: {movieObject.Title} directed by {movieObject.Director}");
+                i++;
+            }
+
+
+            Console.WriteLine("enter the key word: ");
+            string userSearch = Console.ReadLine().ToLower();
+
+            List<MovieClass> searchResult = new List<MovieClass>();
+
+            //int searchResult = 0;
+            foreach (MovieClass movieObject in listOfMovies)
+            {
+                if (movieObject.Title.ToLower().Contains(userSearch))
+                {
+                    //Console.WriteLine($"{movieObject.Title} directed by {movieObject.Director}");
+                    searchResult.Add(movieObject);
+                    // searchResult++;
+                }
+            }
+
+            // Console.WriteLine($"{searchResult} movies found");
+
+            Console.WriteLine($"{searchResult.Count} movies found");
+            foreach (MovieClass movieObjects in searchResult)
+            {
+                Console.WriteLine($"{movieObjects.Title} directed by {movieObjects.Director}");
+            }
+
+        }
+        
+            
+        
+          
+    }
+}
